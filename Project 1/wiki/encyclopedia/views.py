b29be4a09d4f22ff1fcbf6ebb2e84b4dbf231206
yonog1 <yonatangoldin69@gmail.com>
2022-05-06 21:41:22 +0300

    wiki
    completed entry page

---
+++ Project 1/wiki/encyclopedia/views.py
@@ -0,0 +1,27 @@
+from django.http import Http404
+from django.shortcuts import render
+import markdown
+
+from . import util
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+
+def content(request, entry):
+    markdowner = markdown.Markdown()
+    entry_page = util.get_entry(entry)
+    if entry_page is None:
+        return error(request, entry)
+    return render(request, "encyclopedia/content.html", {
+        "entry": entry.capitalize(),
+        "entry_page":markdowner.convert(entry_page)
+    })
+
+def error(request, entry):
+    return render(request, "encyclopedia/error.html", {
+        "entry":entry.capitalize()
+    })
